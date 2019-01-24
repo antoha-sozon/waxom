584bc9f45421774b920b506993d3d44acf4c66d1
lxxnutsxxl <lxxnutsxxl@mail.ru>
2019-01-24 23:28:12 +0200

    Add mobile menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,17 @@
-	$('.head-slider').slick({
-		dots: true,
-		infinite: true,
-		speed: 500,
-		fade: true,
-		cssEase: 'linear'
-	});
+$('.head-slider').slick({
+	dots: true,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	cssEase: 'linear',
+	prevArrow: '<i class="slick-prev icon-chevron-left"></i>',
+	nextArrow: '<i class="slick-next icon-chevron-right"></i>',
+	responsive: [
+		{
+     		breakpoint: 768,
+     		settings: {
+				arrows: false
+	     		}
+ 		}
+	]
+});
